85df701765973ac359df3c55c75d327ec1dda093
PetaQ <smoerrebroed35@web.de>
2014-04-22 21:38:57 +0200

    Add Application Information to Logic

---
+++ Tbx2Csv/Logic/App/AppStatic.cs
@@ -0,0 +1,16 @@
+﻿namespace Tbx2Csv.Logic.App
+{
+    using System.Reflection;
+    using System.Windows;
+    
+    public class AppStatic
+    {
+        public static string ProductVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.Build.ToString();
+            }
+        }
+    }
+}
--- Tbx2Csv/ViewModel/MainWindowViewModel.cs
+++ Tbx2Csv/ViewModel/MainWindowViewModel.cs
@@ -2,12 +2,43 @@
 {
     using System.Windows;
     using Tbx2Csv.DataTypes.DepInjection;
+    using Tbx2Csv.Logic.App;
     
     public class MainWindowViewModel : ViewModelBase, IMainWindowViewModel
     {
+        private string m_tbx2CsvVersion;
+
         public MainWindowViewModel()
         {
             base.DisplayName = "Tbx2Csv";
+            this.InitViewModel();
+        }
+
+        /// <summary>
+        ///     Tbx2Csv Version Property
+        /// </summary>
+        public string Tbx2CsvVersion
+        {
+            get
+            {
+                return this.m_tbx2CsvVersion;
+            }
+            set
+            {
+                if (this.m_tbx2CsvVersion != value)
+                {
+                    this.m_tbx2CsvVersion = value;
+                    this.OnPropertyChanged("Tbx2CsvVersion");
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Init ViewModel
+        /// </summary>
+        private void InitViewModel()
+        {
+            this.Tbx2CsvVersion = AppStatic.ProductVersion;
         }
     }
 }
